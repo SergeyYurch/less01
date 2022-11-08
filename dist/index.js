5e7815f88909a22b1650359eedc6134f31bc6303
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-11-08 10:07:43 +0200

    add script to package.json

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('Hello World! Nodemon Start');
+    res.send('Hello World! Nodemon Start!!!');
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
